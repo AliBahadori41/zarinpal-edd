dc364aeeec9d4b34319c8e5ef97de9d63386dbaa
Ali Bahadori <ali.bahadori41@yahoo.com>
2023-12-24 23:58:21 +0330

    Init commit

---
+++ edd-zarinpal.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: ZarinPal EDD
+ * Author: Ali Bahadori
+ * Description: این افزونه، درگاه پرداخت آنلاین <a href="https://zarinpal.com">زرین‌پال</a> را برای افزونه‌ی Easy Digital Downloads فعال می‌کند.
+ * Version: 2.1.0
+ * Author URI: https://bahadori.dev/
+ */
+
+// Toman Currency
+require 'includes/toman-currency.php';
+
+// Include the main file
+require 'gateways/zarinpal.php';
